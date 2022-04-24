dffe3972201c74e82f7fc54b5f102316b85ee46e
Cihat Gelir <cihatgelir35@gmail.com>
2022-04-24 15:33:18 +0300

    CircleExercise added.

---
+++ src/cihat/javaBackendBeginner/java101/CircleExercise.java
@@ -0,0 +1,24 @@
+package cihat.javaBackendBeginner.java101;
+
+import java.util.Scanner;
+
+/**
+ * Write a program to find the area of a segment of a circle with radius r and central angle a.
+ * Take pi = 3.14.
+ * Formula : (pi * (r*r) * a) / 360
+ * Expect perfect user inputs.
+ * @author Cihat Gelir
+ */
+public class CircleExercise {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Radius : ");
+		float r = scanner.nextFloat();
+		System.out.print("Central angle of segment : ");
+		float a = scanner.nextFloat();
+		
+		float area = (3.14f * (r*r) * a) / 360;
+		System.out.printf("Area of the segment = %.4f" , area);
+		scanner.close();
+	}
+}
