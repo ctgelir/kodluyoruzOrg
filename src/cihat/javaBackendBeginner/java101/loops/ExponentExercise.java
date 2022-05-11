86783375f79516cac2023d42aa19fe5f821cbfe7
Cihat Gelir <cihatgelir35@gmail.com>
2022-05-11 15:57:56 +0300

    ExponentExercise added to java101.loops.

---
+++ src/cihat/javaBackendBeginner/java101/loops/ExponentExercise.java
@@ -0,0 +1,27 @@
+package cihat.javaBackendBeginner.java101.loops;
+
+import java.util.Scanner;
+
+/** @author Cihat Gelir
+ *  Perfect inputs expected.
+ * */
+public class ExponentExercise {
+	public static int exp(int a, int r) {
+		int result = 1;
+		for (int i = 0; i < r; i++) {
+			result *= a;
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("a: ");
+		int a = sc.nextInt();
+		
+		System.out.print("r: ");
+		int r = sc.nextInt();
+		
+		System.out.println(exp(a,r));
+	}
+	
+}
