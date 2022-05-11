f289e2058bf06ed02f4c8fe9048e66adc08022ea
Cihat Gelir <cihatgelir35@gmail.com>
2022-05-11 15:08:45 +0300

    DivisibleByTwelveExercise added to java101.loops.

---
+++ src/cihat/javaBackendBeginner/java101/loops/DivisibleByTwelveExercise.java
@@ -0,0 +1,24 @@
+package cihat.javaBackendBeginner.java101.loops;
+
+import java.util.Scanner;
+
+/**@author Cihat Gelir*/
+public class DivisibleByTwelveExercise {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Number: ");
+		int inp = sc.nextInt();
+		
+		int sum = 0;
+		int numberOfInteger = 0;
+		for (int i = 12; i <= inp; i++) {
+			if (i % 12 == 0) {
+				sum += i;
+				numberOfInteger++;
+			}
+		}
+		float avg = sum / numberOfInteger;
+		System.out.printf("Average of number that are divisible with 12 from  0 to " + inp + ": %.2f", avg);
+		
+	}
+}
