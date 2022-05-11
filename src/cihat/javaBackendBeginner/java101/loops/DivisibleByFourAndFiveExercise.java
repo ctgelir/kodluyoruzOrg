2b93f7321937b1b4b62e82913227b339a4b50134
Cihat Gelir <cihatgelir35@gmail.com>
2022-05-11 15:38:34 +0300

    DivisibleByFourAndFiveExercise added to java101.loops.

---
+++ src/cihat/javaBackendBeginner/java101/loops/DivisibleByFourAndFiveExercise.java
@@ -0,0 +1,18 @@
+package cihat.javaBackendBeginner.java101.loops;
+
+import java.util.Scanner;
+
+/**@author Cihat Gelir*/
+public class DivisibleByFourAndFiveExercise {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Number: ");
+		int inp = sc.nextInt();
+		for (int i = 1; i <= inp; i*=4) {
+			System.out.println(i);
+		}
+		for (int i = 1; i <= inp; i*=5) {
+			System.out.println(i);
+		}
+	}
+}
